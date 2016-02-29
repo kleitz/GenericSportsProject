b60c5aa47a8af558413300a445db3cf1e20831dc
krishna <krishna@hummingbirdinfotech.com>
2016-02-29 13:18:39 +0530

    remove validation from event and sponsor form

--- DesktopModules/ThSport/frmEvent.ascx.cs
+++ DesktopModules/ThSport/frmEvent.ascx.cs
@@ -230,6 +230,14 @@ namespace DotNetNuke.Modules.ThSport
             FillSport();
             FillSeason();
             FillSponsor();
+           
+            FillTeam();
+            FillClub();
+           
+            FillCompetition();
+            FillClubOwner();
+            FillClubMember();
+            FillTeamMember();
         }
 
         protected void btnCloseEvent_Click(object sender, EventArgs e)
@@ -574,26 +582,26 @@ namespace DotNetNuke.Modules.ThSport
 
         protected void ddlClub_SelectedIndexChanged(object sender, EventArgs e)
         {
-            int ClubID = Convert.ToInt32(ddlClub.SelectedValue);
+            //int ClubID = Convert.ToInt32(ddlClub.SelectedValue);
 
-            if (ClubID > 0)
-            {
-                divclubowner.Visible = true;
-                divclubmember.Visible = true;
-                FillClubOwner(ClubID);
-                FillClubMember(ClubID);
-                int SportID = Convert.ToInt32(ddlSports.SelectedValue);
-                FillTeam(SportID, ClubID);
-            }
-            else
-            {
-                divclubowner.Visible = false;
-                divclubmember.Visible = false;
-                FillClubOwner(ClubID);
-                FillClubMember(ClubID);
-                int SportID = Convert.ToInt32(ddlSports.SelectedValue);
-                FillTeam(SportID, ClubID);
-            }
+            //if (ClubID > 0)
+            //{
+            //    divclubowner.Visible = true;
+            //    divclubmember.Visible = true;
+            //    FillClubOwner(ClubID);
+            //    FillClubMember(ClubID);
+            //    int SportID = Convert.ToInt32(ddlSports.SelectedValue);
+            //    FillTeam(SportID, ClubID);
+            //}
+            //else
+            //{
+            //    divclubowner.Visible = false;
+            //    divclubmember.Visible = false;
+            //    FillClubOwner(ClubID);
+            //    FillClubMember(ClubID);
+            //    int SportID = Convert.ToInt32(ddlSports.SelectedValue);
+            //    FillTeam(SportID, ClubID);
+            //}
 
         }
 
@@ -650,47 +658,47 @@ namespace DotNetNuke.Modules.ThSport
 
         protected void ddlSports_SelectedIndexChanged(object sender, EventArgs e)
         {
-            int SportID = Convert.ToInt32(ddlSports.SelectedValue);
+            //int SportID = Convert.ToInt32(ddlSports.SelectedValue);
 
-            if (SportID > 0)
-            {
-                FillCompetition(SportID);
-                FillClub(SportID);
-            }
-            else
-            {
-                divclubmember.Visible = false;
-                divclubowner.Visible = false;
-                divteammember.Visible = false;
-                FillCompetition(SportID);
-                FillClub(SportID);
-            }
+            //if (SportID > 0)
+            //{
+            //    FillCompetition(SportID);
+            //    FillClub(SportID);
+            //}
+            //else
+            //{
+            //    divclubmember.Visible = false;
+            //    divclubowner.Visible = false;
+            //    divteammember.Visible = false;
+            //    FillCompetition(SportID);
+            //    FillClub(SportID);
+            //}
 
         }
 
         protected void ddlTeam_SelectedIndexChanged(object sender, EventArgs e)
         {
-            int TeamID = Convert.ToInt32(ddlTeam.SelectedValue);
+            //int TeamID = Convert.ToInt32(ddlTeam.SelectedValue);
 
-            if (TeamID > 0)
-            {
-                divteammember.Visible = true;
-                FillTeamMember(TeamID);
-            }
-            else
-            {
-                divteammember.Visible = false;
-                FillTeamMember(TeamID);
-            }
+            //if (TeamID > 0)
+            //{
+            //    divteammember.Visible = true;
+            //    FillTeamMember(TeamID);
+            //}
+            //else
+            //{
+            //    divteammember.Visible = false;
+            //    FillTeamMember(TeamID);
+            //}
         }
 
          private void FillCompetition()
          {
-            clsEvent e = new clsEvent();
-            clsEventController ec = new clsEventController();
-            DataTable dt = new DataTable();
+             clsVideos e = new clsVideos();
+             clsVideosController ec = new clsVideosController();
+             DataTable dt = new DataTable();
 
-            dt = ec.FillComptitionIDAndCompetitionName();
+             dt = ec.GetAllCompetition();
             if (dt.Rows.Count > 0)
             {
                 ddlCompetition.DataSource = dt;
